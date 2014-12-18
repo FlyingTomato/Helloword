37076801c278ffbe111fb2605812fa3a0dcbdca6
Harly <harly@AccenturedeMac-mini-2.local>
2014-12-18 15:28:02 +0800

    Initial Commit

---
+++ Helloword/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Helloword
+//
+//  Created by Harly on 14/12/18.
+//  Copyright (c) 2014年 Harly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
